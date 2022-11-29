d99dab210715740982db8a2b859e26aa83563256
Fiona <100845392+fkit00@users.noreply.github.com>
2022-11-29 11:19:24 +0000

    Gonna be honest- kind of smashed it. The only problem i'm having is not displaying you win for long enough- other than that it's pretty awesome

--- main.py
+++ main.py
@@ -4,13 +4,44 @@
 import art 
 import random
 from game_data import data 
+from replit import clear
 #okay we have a random number 
 a= random.randint(0,len(data))
 
 # we are printing the art! fabulous but vs will be in the while loop
-print(art.logo)
-print(art.vs)
-print(a)
-print(data[a])
 
+
+# so in the game the second one becomes the first one in the next round 
 game_over= False
+score = 0
+first_no=random.randint(0,len(data)-1)
+first_id=data[first_no]
+
+while game_over == False:
+  clear()
+  print(art.logo)
+  print(f"The score is {score}")
+  print(f"Compare A: {first_id['name']}, a {first_id['description']},from {first_id['country']}")
+  print(art.vs)
+  second_no=random.randint(0,len(data)-1)
+  second_id=data[second_no] 
+  answer=input(f"Compare B: {second_id['name']}, a {second_id['description']},from {second_id['country']}. Type who has more followers: A or B ").lower()
+  if answer != 'a' and answer != 'b':
+    print('what are you playing at ')
+   
+  elif answer == 'a' and first_id['follower_count']> second_id['follower_count']:
+    print('Congrats you got it right!')
+    first_id=second_id
+    score+=1
+  
+  elif answer == 'b' and second_id['follower_count']> first_id['follower_count']:
+    print('Congrats you got it right!')
+    first_id=second_id
+    score+=1
+   
+  else:
+    print(f"You got it wrong! {first_id['name']} has {first_id['follower_count']} million followers and {second_id['name']} has {second_id['follower_count']} million followers  ")
+    game_over=True
+  
+    
+  
